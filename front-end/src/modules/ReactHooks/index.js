177adaa3d46ce5245899c1ae930e9d4a52bc1cf6
aisah <aisah.tkd@gmail.com>
2023-03-01 20:51:31 +0700

    test1

--- front-end/src/modules/ReactHooks/index.js
+++ front-end/src/modules/ReactHooks/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import useCounter from './components/counterHooks'
 
 function ReactHooks() {
-    const [count, setCount] = useState(0);
+    const [count, increments, decrements] = useCounter(0,1);
 
     useEffect(() => {
         document.title = `you click ${count} times`
@@ -12,8 +13,11 @@ function ReactHooks() {
             <h2>React Hooks</h2>
 
             <p>you clicked {count}</p>
-            <button onClick={() => setCount(count + 1)}>
+            <button onClick={increments}>
                 increase count
+            </button>
+            <button onClick={decrements}>
+                Decrements count
             </button>
         </div>
     )
